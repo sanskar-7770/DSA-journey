1706032144a2cdca95f980f94ba1f62018316c68
Sanskar Pandey <sanskar2416@gmail.com>
2026-07-28 23:20:13 +0530

    remove outermost parenthesis problem

---
+++ strings/Remove Outermost Parentheses.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string result;
+        int level=0;
+        for(char ch:s){
+           if(ch=='('){
+              if(level>0) result+=ch;
+              level++;
+           }
+           else if(ch==')'){
+                level--;
+                if(level>0) result+=ch;
+           }
+        }
+        return result;
+    }
+};
